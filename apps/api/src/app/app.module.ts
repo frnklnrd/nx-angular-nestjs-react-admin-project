a8206b1130f218bde6811591f5efcf713e81d0d5
Franklin RD <frivero.duharte@gmail.com>
2023-12-13 20:46:11 +0100

    Vercel config

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { ConfigModule } from './config/config.module';
 import { AuthModule } from './core/auth/auth.module';
 import { CatsModule } from './resource/cats/cats.module';
 import { TasksModule } from './resource/tasks/tasks.module';
 import { UsersModule } from './resource/users/users.module';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -17,6 +19,12 @@ import { UsersModule } from './resource/users/users.module';
     CatsModule,
     TasksModule,
     // ---------------
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'assets'),
+      exclude: [
+        'api/*',
+      ],
+    })
   ],
   controllers: [
     // AppController
